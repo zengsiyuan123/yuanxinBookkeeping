c6b484e2b8ae5004dc75ede3d871872f724edc88
曾思远 <zengsiyuan@wisonic.com>
2021-12-14 20:04:33 +0800

    feat(): 项目主框架搭建

---
+++ bookkeeping-frontend/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <div class="page_header">
+      <h2>Bookkeeping</h2>
+    </div>
+    <div class="page_main">
+      <Home></Home>
+    </div>
+    <div class="page_footer">
+      <p>底部网站简介</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from '@/components/Home.vue';
+
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+// ::-webkit-scrollbar {
+//   /*滚动条整体样式*/
+//   width: 4px;
+//   /*高宽分别对应横竖滚动条的尺寸*/
+//   height: 160px;
+// }
+
+// ::-webkit-scrollbar-thumb {
+//   /*滚动条里面小方块*/
+//   border-radius: 6px;
+//   box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+//   -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+//   background: #FFF;
+// }
+.page_header {
+  width: 100%;
+  height: 60px;
+  background: #4090FF;
+  padding: 0 30px;
+  box-sizing: border-box;
+  h2 {
+    font-size: 20px;
+    color: #000;
+    font-weight: bold;
+    line-height: 60px;
+  }
+}
+.page_main {
+  width: 100%;
+  height: calc(100vh - 120px);
+}
+.page_footer {
+  width: 100%;
+  height: 60px;
+  background: cadetblue;
+  padding: 0 30px;
+  box-sizing: border-box;
+  p {
+    font-size: 20px;
+    color: #000;
+    font-weight: bold;
+    line-height: 60px;
+  }
+}
+</style>
---
+++ bookkeeping-frontend/src/components/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <div class="container_left">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#1F2D3D"
+        text-color="#BFCBD9"
+        active-text-color="#409EFF">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-s-claim"></i>
+            <span>明细</span>
+          </template>
+          <el-menu-item index="1-1">账单</el-menu-item>
+          <el-menu-item index="1-2">预算</el-menu-item>
+          <el-menu-item index="1-3">资产管家</el-menu-item>
+          <el-menu-item index="1-4">理财知识</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-upload"></i>
+            <span>记账</span>
+          </template>
+          <el-menu-item index="2-1">新增记账</el-menu-item>
+          <el-menu-item index="2-2">账单列表</el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-s-data"></i>
+            <span>图表</span>
+          </template>
+          <el-menu-item index="3-1">周</el-menu-item>
+          <el-menu-item index="3-2">月</el-menu-item>
+          <el-menu-item index="3-3">年</el-menu-item>
+        </el-submenu>
+        <el-submenu index="4">
+          <template slot="title">
+            <i class="el-icon-star-on"></i>
+            <span>日记</span>
+          </template>
+          <el-menu-item index="4-1">欣欣日记</el-menu-item>
+          <el-menu-item index="4-2">远远日记</el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </div>
+
+    <div class="container_right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  display: flex;
+  justify-content: space-between;
+  .container_left {
+    width: 12%;
+    height: 100%;
+    background: #1F2D3D;
+    overflow: auto;
+  }
+}
+</style>
